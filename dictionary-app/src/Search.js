46ab695d2905c3f510e7cfb1e61ca432373aa0a9
KaTeE234 <mashenykamon007@gmail.com>
2022-08-29 13:23:03 +0300

    Added axios and api

--- dictionary-app/src/Search.js
+++ dictionary-app/src/Search.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./Search.css";
+import axios from "axios";
 
 export default function Search() {
   const [word, setWord] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${word}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleWordChange(event) {
     setWord(event.target.value);
