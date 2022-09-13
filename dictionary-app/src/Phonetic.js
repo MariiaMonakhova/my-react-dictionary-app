777e445caf77079c66f6177ff890c5558a494220
KaTeE234 <mashenykamon007@gmail.com>
2022-09-13 13:49:09 +0300

    Corrected mistakes

--- dictionary-app/src/Phonetic.js
+++ dictionary-app/src/Phonetic.js
@@ -4,7 +4,12 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="phonetic mt-3">
-      <a href={props.phonetic.audio} target="_blank" className="mt-2">
+      <a
+        href={props.phonetic.audio}
+        target="_blank"
+        rel="noreferrer"
+        className="mt-2"
+      >
         PLAY
       </a>
       <span>{props.phonetic.text}</span>
